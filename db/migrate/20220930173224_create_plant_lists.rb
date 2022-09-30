3267affec431ce8c4b7b8f01d74e9695122cbf5a
nessa kodo <nessakodo@gmail.com>
2022-09-30 11:43:46 -0600

    added

--- db/migrate/20220930173224_create_plant_lists.rb
+++ db/migrate/20220930173224_create_plant_lists.rb
@@ -2,8 +2,8 @@ class CreatePlantLists < ActiveRecord::Migration[6.1]
   def change
     create_table :plant_lists do |t|
       t.integer :farmer_id
-      t.array :unlocked_plants  
-      t.array :locked_plants
+      t.string :unlocked_plants  
+      t.string :locked_plants
     end
   end
 end
